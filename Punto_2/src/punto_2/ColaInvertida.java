6c4ffb0394a2cd50e9a3a7ebdd67d56185c16f03
danirirom3-hub <danirirom30@gmail.com>
2025-10-31 12:23:02 -0500

    Add files via upload

---
+++ Punto_2/src/punto_2/ColaInvertida.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package punto_2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ColaInvertida {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> colaA = new LinkedList<>();
+        Queue<Integer> colaB = new LinkedList<>();
+        Stack<Integer> pila = new Stack<>();
+
+        System.out.print("¿Cuántos números quieres ingresar en la Cola A? ");
+        int n = sc.nextInt();
+
+        // Ingresar datos a la cola A
+        for (int i = 0; i < n; i++) {
+            System.out.print("Ingresa el número " + (i + 1) + ": ");
+            int num = sc.nextInt();
+            colaA.add(num);
+        }
+
+        System.out.println("\nCola A inicial: " + colaA);
+
+        // Pasar elementos de A a la pila
+        while (!colaA.isEmpty()) {
+            pila.push(colaA.remove());
+        }
+
+        // Pasar elementos de la pila a la cola B
+        while (!pila.isEmpty()) {
+            colaB.add(pila.pop());
+        }
+
+        System.out.println("Cola B (orden invertido): " + colaB);
+        System.out.println("Cola A al final: " + colaA);
+    }
+}
